cd58c857b3b9428512316ae64fab48b33daa487c
Johan Andersson <johan.rejeep@gmail.com>
2013-12-09 09:18:45 +0100

    Add Ecukes.

--- Cask
+++ Cask
@@ -1,3 +1,7 @@
 (source melpa)
 
 (package-file "prodigy.el")
+
+(development
+ (depends-on "f")
+ (depends-on "ecukes"))
---
+++ features/support/env.el
@@ -0,0 +1,14 @@
+(require 'f)
+
+(defvar prodigy-support-path
+  (f-dirname load-file-name))
+
+(defvar prodigy-features-path
+  (f-parent prodigy-support-path))
+
+(defvar prodigy-root-path
+  (f-parent prodigy-features-path))
+
+(require 'prodigy (f-expand "prodigy" prodigy-root-path))
+(require 'espuds)
+(require 'ert)
