a20e214af43c7814617020852e668e34d60a5795
Johan Andersson <johan.rejeep@gmail.com>
2013-12-13 12:24:52 +0100

    Adding basic tags support.

--- features/step-definitions/prodigy-steps.el
+++ features/step-definitions/prodigy-steps.el
@@ -25,11 +25,14 @@
 
 (Given "^I add the following services:$"
   (lambda (table)
-    (let ((head (car table))
-          (rows (cdr table)))
+    (let* ((head (car table))
+           (rows (cdr table))
+           (name-index (-elem-index "name" head))
+           (tags-index (-elem-index "tags" head)))
       (dolist (row rows)
         (prodigy-define-service
-          :name (nth 0 row))))))
+          :name (nth name-index row)
+          :tags (and tags-index (read (nth tags-index row))))))))
 
 (Then "^I should see services:$"
   (lambda (table)
@@ -37,6 +40,7 @@
            (name-index (-elem-index "name" head))
            (highlighted-index (-elem-index "highlighted" head))
            (marked-index (-elem-index "marked" head))
+           (tags-index (-elem-index "tags" head))
            (rows (cdr table)))
       (save-excursion
         (goto-char (point-min))
@@ -49,12 +53,19 @@
                     (read (nth highlighted-index row))))
                  (marked
                   (when marked-index
-                    (read (nth marked-index row)))))
+                    (read (nth marked-index row))))
+                 (tags
+                  (when tags-index
+                    (read (nth tags-index row)))))
              (let ((line (buffer-substring-no-properties (line-beginning-position)
                                                          (line-end-position))))
                (if marked
                    (should (s-starts-with? (concat "* " name) line))
-                 (should (s-starts-with? (concat "  " name) line))))
+                 (should (s-starts-with? (concat "  " name) line)))
+               (let ((match (s-matches? (format "\\[%s\\]" (s-join ", " (-map 'symbol-name tags))) line)))
+                 (if tags
+                     (should match)
+                   (should-not match))))
              (when highlighted
                (should (eq (get-text-property (1+ (line-beginning-position)) 'face) 'prodigy-line-face))
                (should (eq (get-text-property (line-end-position) 'face) 'prodigy-line-face)))
