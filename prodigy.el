7379ddfe6a58b5fc12977e0c8d047a9e24c82218
Johan Andersson <johan.rejeep@gmail.com>
2014-08-11 10:14:53 +0200

    Release v0.6.0-cvs.

--- prodigy.el
+++ prodigy.el
@@ -4,7 +4,7 @@
 
 ;; Author: Johan Andersson <johan.rejeep@gmail.com>
 ;; Maintainer: Johan Andersson <johan.rejeep@gmail.com>
-;; Version: 0.6.0
+;; Version: 0.6.0-cvs
 ;; URL: http://github.com/rejeep/prodigy.el
 ;; Package-Requires: ((s "1.8.0") (dash "2.4.0") (f "0.14.0") (emacs "24"))
 
