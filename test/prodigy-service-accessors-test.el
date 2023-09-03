ccb9db9c5f03af380ead09d9bd660b5a3f183b99
Damien Cassou <damien@cassou.me>
2023-09-03 10:54:07 +0200

    Update Copyright

--- test/prodigy-service-accessors-test.el
+++ test/prodigy-service-accessors-test.el
@@ -1,6 +1,6 @@
 ;;; prodigy-service-accessors-test.el --- Prodigy: Tests for the public API -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2014-2022 Johan Andersson
+;; Copyright (C) 2014-2023 Johan Andersson
 
 ;; Author: Johan Andersson <johan.rejeep@gmail.com>
 ;; Maintainer: Johan Andersson <johan.rejeep@gmail.com>
